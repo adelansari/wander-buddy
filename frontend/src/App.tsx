c15cf9327d9415528d343d46817173056c6e2e8a
adelansari <adelansari.a@gmail.com>
2024-05-23 21:18:01 +0300

    Refactor App component to use react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import { Footer } from './components/layouts/Footer';
-import { Button } from './components/ui/button';
-import { ModeToggle } from './components/ui/mode-toggle';
+import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { router } from './Router';
 
 function App() {
   return (
     <>
       <ThemeProvider storageKey='vite-ui-theme'>
-        <ModeToggle />
-        <Button>Click me</Button>
-        <Footer />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </>
   );
