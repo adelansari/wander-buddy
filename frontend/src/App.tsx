d2fade8c0ef96cff45d96a789d03b99b21d78169
adelansari <adelansari.a@gmail.com>
2024-05-23 19:47:46 +0300

    Add useTheme hook and ModeToggle component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { Footer } from './components/layouts/Footer';
 import { Button } from './components/ui/button';
+import { ModeToggle } from './components/ui/mode-toggle';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
     <>
-      <Button>Click me</Button>
+      <ThemeProvider storageKey='vite-ui-theme'>
+        <ModeToggle />
+        <Button>Click me</Button>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
