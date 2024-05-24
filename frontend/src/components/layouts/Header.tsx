c5c5414a3c20744e8e5284b800c68e6524edb358
Dana Popa <popa.dana100@gmail.com>
2024-05-24 18:05:51 +0300

    Add Logo image in the header

--- frontend/src/components/layouts/Header.tsx
+++ frontend/src/components/layouts/Header.tsx
@@ -1,11 +1,19 @@
 import { PageHeader, PageHeaderHeading } from '../page-header';
+import logo from '../../assets/logoTravelBuddy.svg';
 
 export const Header = () => {
   return (
     <>
       <header className='flex items-center justify-center gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row'>
         <PageHeader>
-          <PageHeaderHeading className='text-4xl mb-4'>Wander Buddy</PageHeaderHeading>
+          <div className="flex flex-col items-center">
+            <img 
+              src={logo} 
+              alt="Travel Buddy Logo" 
+              className="h-10 w-10 md:h-12 md:w-12 filter dark:invert mb-2" 
+            />
+            <PageHeaderHeading className='text-4xl mb-4'>Wander Buddy</PageHeaderHeading>
+          </div>
         </PageHeader>
       </header>
     </>
