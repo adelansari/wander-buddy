e64745b659a624a722a2856828aacdd38232ea3b
adelansari <adelansari.a@gmail.com>
2024-05-25 12:54:13 +0300

    restructuring folders and files

--- frontend/src/components/layouts/AppLayout.tsx
+++ frontend/src/components/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { ModeToggle } from '../mode-toggle';
+import { ModeToggle } from '../custom/themes/mode-toggle';
 import Logo from '../../assets/logoTravelBuddy.svg';
 
 export function Applayout() {
