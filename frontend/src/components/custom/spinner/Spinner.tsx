1b13dd79584d9e2da6e9e000baf799da18c1566c
adelansari <adelansari.a@gmail.com>
2024-06-01 08:37:24 +0300

    Update Spinner component styling to be centered

--- frontend/src/components/custom/spinner/Spinner.tsx
+++ frontend/src/components/custom/spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import logo from '@/assets/wander-app-logo-v1.svg';
 
 const Spinner: FC = () => (
-  <div className='relative flex justify-center items-center'>
+  <div className='fixed inset-0 flex justify-center items-center'>
     <div className='absolute animate-spin rounded-full h-32 w-32 border-t-8 border-b-8 border-cyan-500'></div>
     <img src={logo} className='rounded-full h-16 w-16 filter dark:invert' />
   </div>
