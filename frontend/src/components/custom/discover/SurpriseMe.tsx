d6f93d2da0aac381f370ce1d335571d9043ac85c
adelansari <adelansari.a@gmail.com>
2024-06-04 18:55:36 +0300

    Refactor SurpriseMe component and make it concise.

--- frontend/src/components/custom/discover/SurpriseMe.tsx
+++ frontend/src/components/custom/discover/SurpriseMe.tsx
@@ -9,15 +9,12 @@ const SurpriseMe = () => {
   const [imagesLoaded, setImagesLoaded] = useState<boolean>(false);
   const [selectedCity, setSelectedCity] = useState<string>('');
   const [citySummary, setCitySummary] = useState<string>('');
-  const [loadError, setLoadError] = useState<boolean>(false);
   const navigate = useNavigate();
 
-  const selectRandomCity = async () => {
+  const fetchRandomCityData = async () => {
     const randomCity = destinationDataLong[Math.floor(Math.random() * destinationDataLong.length)];
     setSelectedCity(randomCity);
     setImagesLoaded(false);
-    setLoadError(false);
-
     try {
       const summary = await wikipedia.summary(randomCity);
       setCitySummary(summary.extract);
@@ -26,59 +23,43 @@ const SurpriseMe = () => {
     }
   };
 
+  const navigateToCityDetails = () => navigate(`/discover/details/${selectedCity}`);
+
   useEffect(() => {
-    selectRandomCity();
+    fetchRandomCityData();
   }, []);
-
-  const handleImageLoad = () => setImagesLoaded(true);
-  const handleImageError = () => setLoadError(true);
-
-  const handleGridItemClick = (city: string) => {
-    navigate(`/discover/details/${city}`);
-  };
 
   return (
     <div className='flex flex-col items-center justify-center'>
       <h2 className='text-2xl font-bold mb-4 text-center'>Feeling Adventurous?</h2>
-
       <div className='grid md:grid-cols-2 gap-6 w-full bg-gray-100 dark:bg-gray-900 p-6 rounded-md shadow-lg'>
-        {selectedCity ? (
-          <div
-            className='relative h-72 w-full mx-auto rounded-xl shadow-xl overflow-hidden cursor-pointer transform group hover:scale-105 transition-transform duration-300 flex items-center justify-center'
-            onClick={() => handleGridItemClick(selectedCity)}
-            role='button'
-            aria-label={`Explore ${selectedCity}`}
-            tabIndex={0}
-          >
-            {!imagesLoaded && !loadError && <Spinner />}
-            <img
-              src={`https://source.unsplash.com/400x400/?${selectedCity}`}
-              alt={selectedCity}
-              onLoad={handleImageLoad}
-              onError={handleImageError}
-              className={`w-full h-full object-cover transition-opacity duration-500 ${
-                imagesLoaded ? 'opacity-100' : 'opacity-0'
-              }`}
-            />
-            {imagesLoaded && !loadError && (
-              <div className='absolute bottom-0 left-0 right-0 flex justify-center items-center bg-black bg-opacity-60 text-white p-2 font-serif text-xl font-bold transform translate-y-full group-hover:translate-y-0 transition-transform duration-500'>
-                {selectedCity}
-              </div>
-            )}
-            {loadError && (
-              <div className='absolute inset-0 flex justify-center items-center bg-gray-200 text-gray-500'>
-                Image failed to load
-              </div>
-            )}
-          </div>
-        ) : (
-          <Spinner />
-        )}
+        <div
+          className='relative h-72 w-full mx-auto rounded-xl shadow-xl overflow-hidden cursor-pointer transform group hover:scale-105 transition-transform duration-300 flex items-center justify-center'
+          onClick={navigateToCityDetails}
+          role='button'
+          aria-label={`Explore ${selectedCity}`}
+          tabIndex={0}
+        >
+          {!imagesLoaded && <Spinner />}
+          <img
+            src={`https://source.unsplash.com/400x400/?${selectedCity}`}
+            alt={selectedCity}
+            onLoad={() => setImagesLoaded(true)}
+            className={`w-full h-full object-cover transition-opacity duration-500 ${
+              imagesLoaded ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+          {imagesLoaded && (
+            <div className='absolute bottom-0 left-0 right-0 flex justify-center items-center bg-black bg-opacity-60 text-white p-2 font-serif text-xl font-bold transform translate-y-full group-hover:translate-y-0 transition-transform duration-500'>
+              {selectedCity}
+            </div>
+          )}
+        </div>
         {citySummary && (
           <div className='p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md h-72 overflow-y-scroll'>
             <h1
               className='text-2xl font-semibold mb-2 cursor-pointer hover:underline'
-              onClick={() => handleGridItemClick(selectedCity)}
+              onClick={navigateToCityDetails}
               tabIndex={0}
               role='button'
               aria-label={`Explore ${selectedCity}`}
@@ -89,9 +70,8 @@ const SurpriseMe = () => {
           </div>
         )}
       </div>
-
       <div className='mt-4'>
-        <Button onClick={selectRandomCity}>Show Random City</Button>
+        <Button onClick={fetchRandomCityData}>Show Random City</Button>
       </div>
     </div>
   );
